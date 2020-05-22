fdc33161654fba8c52717bbe82ce36bdf33788f3
sarliss <sinaramartins@gmail.com>
2020-05-22 01:14:06 -0400

    Added comments controler

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,48 @@
 class CommentsController < ApplicationController
+  before_action :set_article
+  before_action :set_article_comment, only: [:show, :update, :destroy]
+
+  # GET /articles/:article_id/comments
+  def index
+    json_response(@article.comments)
+  end
+
+  # GET /articles/:article_id/comments/:id
+  def show
+    json_response(@article)
+  end
+
+  # POST /articles/:article_id/comments
+  def create
+    @article.comments.create!(comment_params)
+    # json_response(@article)
+    json_response(status: 'SUCCESS', message: 'comment created successfully.')
+
+  end
+
+  # PUT /articles/:article_id/comments/:id
+  def update
+    @comment.update(comment_params)
+    json_response(status: 'SUCCESS', message: 'comment updated successfully.')
+  end
+
+  # DELETE /articles/:article_id/comments/:id
+  def destroy
+    @comment.destroy
+    json_response(status: 'SUCCESS', message: 'comment deleted successfully.')
+  end
+
+  private
+
+  def comment_params
+    params.permit(:description)
+  end
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def set_article_comment
+    @comment = @article.comment.find_by!(id: params[:id]) if @article
+  end
 end
