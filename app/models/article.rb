ef9d47afb69218ca32a834ed1ef6de3c9b1135c0
sarliss <sinaramartins@gmail.com>
2020-05-21 22:14:01 -0400

    Added model associations and controllers

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  # model association
+  has_many :comments, dependent: :destroy
+
+  # validations
+  validates_presence_of :title
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  # validation
+  validates_presence_of :description
 end
