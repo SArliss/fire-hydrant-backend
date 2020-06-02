a285fc7d60aea028097cd8831e4794dcb9086e87
sarliss <sinaramartins@gmail.com>
2020-06-02 11:22:56 -0400

    Added seeds test file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(name: "sinara", email: "sinara@email.com", password: "1234", "password_confirmation": "1234")
+User.create(name: "jeremy", email: "jeremy@email.com", password: "1234", "password_confirmation": "1234")
+User.create(name: "mike", email: "mike@email.com", password: "1234", "password_confirmation": "1234")
+User.create(name: "simone", email: "simone@email.com", password: "1234", "password_confirmation": "1234")
+User.create(name: "guest", email: "guest@email.com", password: "1234", "password_confirmation": "1234")
+
