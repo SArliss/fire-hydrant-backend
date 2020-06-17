227eec789bbf54190a631b8f10dce8fdd57bdcbe
sarliss <sinaramartins@gmail.com>
2020-06-16 21:53:28 -0400

    Added more users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,10 @@
 
 User.create(
   name: "Sinara Arliss", 
-  email: "sinara@email.com", 
-  location: "New Jersey", 
+  email: "sinaraarliss@gmail.com", 
+  location: "New Jersey, USA", 
   position: "Solutions Engineer", 
-  fun_facts: "I have a labradoodle named Chico who thinks he's a human", 
+  fun_facts: "I love solving puzzles, I speak English and Portuguese fluenty and I have a playful labradoodle named Chico", 
   first_img: "https://www.sinaraarliss.com/static/media/Sinara.c567cb99.jpg",
   second_img: "https://i.imgur.com/eiT3DKY.jpg",
   password: "1234"
@@ -23,9 +23,9 @@ User.create(
   User.create(
     name: "Benjamin Martins", 
     email: "benjamin@email.com", 
-    location: "New Jersey", 
+    location: "New Jersey, USA", 
     position: "Solutions Engineer", 
-    fun_facts: "I eat blueberries every day", 
+    fun_facts: "I like to eat blueberries for breakfast every day. My favorite television show is Monster Jam", 
     first_img: "https://randomuser.me/api/portraits/men/18.jpg",
     second_img: "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg",
     password: "1234"
@@ -34,20 +34,20 @@ User.create(
   User.create(
   name: "Harvey Garrett", 
   email: "harvey@email.com", 
-  location: "New York", 
+  location: "New York, USA", 
   position: "Sales Engineer", 
-  fun_facts: "I like to surf very early in the morning", 
+  fun_facts: "I like to surf very early in the morning even during the winter months. I also play hockey", 
   first_img: "https://randomuser.me/api/portraits/men/51.jpg",
   second_img: "https://cdn.pixabay.com/photo/2017/05/06/10/32/dog-2289451_1280.jpg",
   password: "1234"
   )
 
   User.create(
-  name: "Maria Lucia", 
-  email: "maria@email.com", 
-  location: "New York", 
+  name: "Ayako Chun", 
+  email: "ayako@email.com", 
+  location: "New York, USA", 
   position: "UX Designer", 
-  fun_facts: "I enjoy baking and paiting", 
+  fun_facts: "I enjoy baking. According to my friends I make the best apple pie!", 
   first_img: "https://randomuser.me/api/portraits/women/90.jpg",
   second_img: "https://cdn.pixabay.com/photo/2016/02/19/15/46/dog-1210559_1280.jpg",
   password: "1234"
@@ -56,29 +56,29 @@ User.create(
   User.create(
     name: "Stella Ellis", 
     email: "stella@email.com", 
-    location: "New York", 
+    location: "New York, USA", 
     position: "UX Designer", 
     fun_facts: "I love traveling and I have visited 125 countries", 
-    first_img: "https://randomuser.me/api/portraits/women/23.jpg",
+    first_img: "https://randomuser.me/api/portraits/women/0.jpg",
     second_img: "https://cdn.pixabay.com/photo/2016/01/19/17/54/dog-1149964_1280.jpg",
     password: "1234"
   )
 
   User.create(
-    name: "Ruben Leclercq", 
+    name: "Ruben Dubois", 
     email: "ruben@email.com", 
-    location: "Paris", 
+    location: "Paris, France", 
     position: "Data Engineer", 
-    fun_facts: "I love traveling and I have visited 125 countries", 
+    fun_facts: "I have a really big heart, and I empathize with others very easily", 
     first_img: "https://randomuser.me/api/portraits/men/80.jpg",
     second_img: "https://cdn.pixabay.com/photo/2016/11/21/00/47/view-1844110_1280.jpg",
     password: "1234"
   )
 
   User.create(
-    name: "John Davis", 
-    email: "john@email.com", 
-    location: "Paris", 
+    name: "Thomas Bernard", 
+    email: "thomas@email.com", 
+    location: "Paris, France", 
     position: "Data Engineer", 
     fun_facts: "I love playing old school videogames, my favorite game is donkey kong", 
     first_img: "https://randomuser.me/api/portraits/men/85.jpg",
@@ -87,34 +87,56 @@ User.create(
   )
 
   User.create(
-    name: "Violet Silva", 
+    name: "Violet Jones", 
     email: "violet@email.com", 
-    location: "Florida", 
+    location: "Florida, USA", 
     position: "Software Engineer", 
-    fun_facts: "I like to listen to bossa nova music", 
+    fun_facts: "There are 12 seasons and 293 episodes of Grey’s Anatomy and I have seen every single one", 
     first_img: "https://randomuser.me/api/portraits/women/63.jpg",
     second_img: "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
     password: "1234"
   )
 
   User.create(
-    name: "Mike Santos", 
+    name: "Mike Smith", 
     email: "mike@email.com", 
-    location: "California", 
+    location: "California, USA", 
     position: "Software Engineer", 
-    fun_facts: "I'm a cat person and I speak 6 languages", 
+    fun_facts: "I'm a cat person, I speak 6 languages and I'm allergic to shellfish", 
     first_img: "https://randomuser.me/api/portraits/men/35.jpg",
     second_img: "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg",
     password: "1234"
   )
 
   User.create(
-    name: "guest", 
+    name: "Antonio Santos", 
+    email: "antonio@email.com", 
+    location: "Sao Paulo, Brazil", 
+    position: "Marketing Manager", 
+    fun_facts: "I like to listen to bossa nova music and I enjoy playing beach volleyball", 
+    first_img: "https://randomuser.me/api/portraits/men/10.jpg",
+    second_img: "https://cdn.pixabay.com/photo/2020/05/15/16/36/parrot-5174193_1280.jpg",
+    password: "1234"
+  )
+
+  User.create(
+    name: "Beatriz Oliveira", 
+    email: "beatriz@email.com", 
+    location: "Sao Paulo, Brazil", 
+    position: "Product Manager", 
+    fun_facts: "I love to sing and play piano. I would rather shop for musical instruments than clothes or shoes", 
+    first_img: "https://randomuser.me/api/portraits/women/26.jpg",
+    second_img: "https://cdn.pixabay.com/photo/2017/09/09/18/39/lizard-2732989_1280.jpg",
+    password: "1234"
+  )
+
+  User.create(
+    name: "Guest", 
     email: "guest@email.com", 
-    location: "New York", 
-    position: "Software Engineer", 
+    location: "New York, USA", 
+    position: "Technical Recruiter", 
     fun_facts: "I'm a curious and self-driven individual", 
-    first_img: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+    first_img: "https://cdn.pixabay.com/photo/2017/03/31/17/45/avatar-2191935_1280.png",
     second_img: "https://cdn.pixabay.com/photo/2013/11/01/11/13/dolphin-203875_1280.jpg",
     password: "1234"
   )
